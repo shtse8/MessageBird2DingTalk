c42a3afc483d6ae85bdfdf764cf115274a4d3311
Kyle Tse <shtse8@gmail.com>
2018-09-29 01:13:56 +0800

    First working version

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
+const express = require('express')
+const bodyParser = require("body-parser");
+const axios = require('axios');
+const app = express()
+const port = process.env.PORT || 80
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+	axios.post('https://oapi.dingtalk.com/robot/send?access_token=7903f40a61ee9241b72b3102717a5d61f377805f3c2b22aa5274cbc66cba499d', 	{
+		"msgtype": "text",
+		"text": {
+			"content": JSON.stringify(req.body)
+		}
+	})
 });
-server.listen(process.env.PORT);
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
